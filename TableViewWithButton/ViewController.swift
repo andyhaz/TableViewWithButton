220c7d47c7f92ff5d8f52edd0419f60e4c9f2d63
andrew <andyhaz1970@gmail.com>
2022-12-20 15:34:34 -0600

    Initial Commit

---
+++ TableViewWithButton/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TableViewWithButton
+//
+//  Created by andyhaz on 12/20/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
